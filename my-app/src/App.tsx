8313818443095a3b97d72c3cd18cd46f92bdd96a
NovichenkoYura <novichenko.y@gmail.com>
2022-10-28 15:45:17 +0300

    card

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,23 +1,6 @@
 import React from 'react';
 import './App.css';
 
-import { Routes, Route, Link } from 'react-router-dom';
-import { Main } from './pages/Main';
-
-import { locations } from '../src/constants/locations';
-
-import { Subscribers } from './pages/Subscribers';
-import { Subscriptions } from './pages/Subscriptions';
-import { Messages } from './pages/Messages';
-import { Saved } from './pages/Saved';
-import { BecomeAnOwner } from './pages/BecomeAnOwner';
-import { NotFoundpage } from './pages/NotFoundpage';
-import { Registration } from './pages/Registration';
-
-import { Layout } from './components/Layout';
-import { Login } from './pages/Login';
-import { Wishlist } from './pages/Wishlist';
-import { Basket } from './pages/Basket';
 import { ComponentRoutes } from './constants/componentRoutes';
 
 function App() {
