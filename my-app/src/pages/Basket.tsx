1833856505fb9aeed291ff1f256c7c6a0efe0425
NovichenkoYura <novichenko.y@gmail.com>
2022-11-11 06:48:32 +0200

    arr

--- my-app/src/pages/Basket.tsx
+++ my-app/src/pages/Basket.tsx
@@ -6,12 +6,57 @@ const Basket = () => {
   console.log('basketId', basketId);
   console.log('tours', tours);
 
-  const toursInBasket = tours.filter((tour) => tour.id === basketId);
+  const toursInBasket = tours.filter((tour) => basketId.includes(tour.id));
 
   return (
-    <div>
-      <h1>Shopping Cart</h1>
-    </div>
+    <>
+      <ul>
+        {toursInBasket.map((tour) => (
+          <li className="tours__item" key={tour.id}>
+            <div className="wrap">
+              <div className="toursItemImage__container">
+                <img src={tour.imgsourse} className="toursItem__image" alt="Meal" />
+                <img src={tour.imgsourse} className="toursItem__image" alt="Pig" />
+              </div>
+
+              <div className="tour__text-part">
+                <h2 className="tour__title">{tour.name}</h2>
+                <p className="tour__generaldecsription">{tour.description}</p>
+                <div className="tourData__forFilter">
+                  <a className="tourData__forFilter__item">
+                    <span>Duration: </span>
+                    {tour.duration}
+                    <span> days</span>
+                  </a>
+                  <a className="tourData__forFilter__item">{tour.country}</a>
+                  <a className="tourData__forFilter__item">
+                    <span>Budget for 1 person: </span>
+                    {tour.budget} <span>Euro</span>
+                  </a>
+                  <a className="tourData__forFilter__item">Feedback</a>
+                </div>
+              </div>
+            </div>
+            <div className="tours__btns">
+              {/* <button
+                onClick={() => dispatch(addtoBasketThunk(tour.id))}
+                className="cardTours__btn">
+                Add to
+                <Basket className="tourCard__svgbasket" width="25" height="25" />
+              </button> */}
+              {/* <button className="cardTours__btn">More...</button>
+              <div onClick={() => setWishItem(!wishItem)}>
+                {wishItem ? (
+                  <Chosen className="tourCard__svgwishlist" width="25" height="25" />
+                ) : (
+                  <NotChosen className="tourCard__svgwishlist" width="25" height="25" />
+                )}
+              </div> */}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
